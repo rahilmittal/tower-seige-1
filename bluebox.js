57428a13b38de50773c452280b5476c812551615
rahilmittal <74299504+rahilmittal@users.noreply.github.com>
2021-01-27 17:32:48 +0530

    Add files via upload

---
+++ bluebox.js
@@ -0,0 +1,13 @@
+class Box extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill("blue");
+    super.display();
+  }
+}
---
+++ pinkbox.js
@@ -0,0 +1,14 @@
+class Box1 extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill("pink");
+        super.display();
+  }
+}
+     
